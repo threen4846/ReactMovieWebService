1b05d1b153962ed75c24d4a519aa2fcc15a3200e
threen4846 <threen4846@naver.com>
2023-01-17 02:33:04 +0900

    react-router-dom버전 수정

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Detail from "./routes/Detail";
 import Home from "./routes/Home";
 
 function App() {
   return (
     <Router>
-      <Routes>
-        <Route path="abot-us" element={<h1>Hello</h1>} />
-        <Route path={`${process.env.PUBLIC_URL}/`} element={<Home />} />
-        <Route path="/movie/:id" element={<Detail />} />
-      </Routes>
+      <Switch>
+        <Route path="/hello">
+          <h1>Hello</h1>
+        </Route>
+        <Route path="/movie">
+          <Detail />
+        </Route>
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
     </Router>
   );
 }
